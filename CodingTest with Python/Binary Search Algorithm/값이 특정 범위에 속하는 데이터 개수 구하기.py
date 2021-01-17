7383e744866aa0b39079f4f97b2665d7f8d35b18
Lee ChaeHyeon <chlee1001@naver.com>
2021-01-17 19:38:13 +0900

    21.01.17_Binary_Search_Algorithm

---
+++ CodingTest with Python/Binary Search Algorithm/값이 특정 범위에 속하는 데이터 개수 구하기.py
@@ -0,0 +1,18 @@
+from bisect import bisect_left, bisect_right
+
+
+# 값이 [left_value, right_value]인 데이터의 개수를 반환하는 함수 >> left_value 이상 right_value 이하
+def count_by_value(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+
+# 배열 선언
+a = [1, 2, 3, 3, 3, 3, 4, 4, 8, 9]
+
+# 값이 4인 데이터 개수 출력
+print(count_by_value(a, 4, 4))
+
+# 값이 [-1, 3] 범위에 있는 데이터 개수 출력
+print(count_by_value(a, -1, 3))
