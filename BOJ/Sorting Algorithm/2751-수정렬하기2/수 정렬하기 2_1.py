2c072d01bf6bbb173f0a6b2d9f0d9f93f2c789ff
Lee ChaeHyeon <chlee1001@naver.com>
2021-02-27 22:07:48 +0900

    21.02.27_BOJ_2751-수 정렬하기 2

--- BOJ/Sorting Algorithm/2751-수정렬하기2/수 정렬하기 2_1.py
+++ BOJ/Sorting Algorithm/2751-수정렬하기2/수 정렬하기 2_1.py
@@ -1,7 +1,8 @@
 import sys
+
 n = int(input())
 l = []
 for i in range(n):
     l.append(int(sys.stdin.readline()))
 for i in sorted(l):
-    sys.stdout.write(str(i)+'\n')
+    sys.stdout.write(str(i) + '\n')
---
+++ BOJ/Sorting Algorithm/2751-수정렬하기2/수 정렬하기 2_2.py
@@ -0,0 +1,49 @@
+import sys
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+
+def merge_sort(array):
+    if len(array) <= 1:
+        return array
+    mid = len(array) // 2
+    left = merge_sort(array[:mid])
+    right = merge_sort(array[mid:])
+
+    i, j, k = 0, 0, 0
+
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            array[k] = left[i]
+            i += 1
+        else:
+            array[k] = right[j]
+            j += 1
+        k += 1
+
+    if i == len(left):
+        while j < len(right):
+            array[k] = right[j]
+            j += 1
+            k += 1
+    elif j == len(right):
+        while i < len(left):
+            array[k] = left[i]
+            i += 1
+            k += 1
+    return array
+
+
+# 데이터 입력
+n = int(input())
+num = []
+
+for _ in range(n):
+    num.append(int(input()))
+
+num = merge_sort(num)
+
+for x in num:
+    print(str(x) + '\n')
+
