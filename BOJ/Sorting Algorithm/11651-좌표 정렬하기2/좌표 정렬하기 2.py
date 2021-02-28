b6c1fafd5d09eee32b26279f9de02fe88726aace
Lee ChaeHyeon <chlee1001@naver.com>
2021-03-01 00:00:21 +0900

    21.03.01_BOJ_11651-좌표정렬하기2

---
+++ BOJ/Sorting Algorithm/11651-좌표 정렬하기2/좌표 정렬하기 2.py
@@ -0,0 +1,15 @@
+import sys
+
+input = sys.stdin.readline
+
+n = int(input())
+
+coord_list = []
+
+for _ in range(n):
+    coord_list.append(list(map(int, input().split())))
+
+coord_list.sort(key=lambda coord: (coord[1], coord[0]))
+
+for coord in coord_list:
+    print(coord[0], coord[1])
