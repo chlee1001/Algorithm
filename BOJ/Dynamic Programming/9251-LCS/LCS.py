15904845ebc092b03c0a3dd14de2f0a18ef2dbf4
Lee ChaeHyeon <chlee1001@naver.com>
2021-03-22 00:24:19 +0900

    21.03.22_BOJ_9251-LCS

---
+++ BOJ/Dynamic Programming/9251-LCS/LCS.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+str1 = ' ' + input().rstrip()
+str2 = ' ' + input().rstrip()
+
+dp = [[0] * len(str2) for _ in range(len(str1))]
+
+for i in range(1, len(str1)):
+    for j in range(1, len(str2)):
+        if str1[i] == str2[j]:
+            dp[i][j] = dp[i - 1][j - 1] + 1
+        else:
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
+
+print(dp[-1][-1])
