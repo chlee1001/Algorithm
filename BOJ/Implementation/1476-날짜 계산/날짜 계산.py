0293d667c9db2adc4183c010e5cf13cef018db8a
Lee ChaeHyeon <chlee1001@naver.com>
2021-04-19 20:31:57 +0900

    21.04.19_BOJ_1476번-날짜 계산

---
+++ BOJ/Implementation/1476-날짜 계산/날짜 계산.py
@@ -0,0 +1,13 @@
+import sys
+
+input = sys.stdin.readline
+
+E, S, M = map(int, input().split())
+
+year = 1
+
+while True:
+    if (year - E) % 15 == 0 and (year - S) % 28 == 0 and (year - M) % 19 == 0:
+        print(year)
+        break
+    year += 1
