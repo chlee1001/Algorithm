6982b2bc5d7a5d96e4e04883f9e67afea52adace
Lee ChaeHyeon <chlee1001@naver.com>
2021-07-18 00:05:38 +0900

    21.07.18_PROGRAMMERS_Lv2.주식가격

---
+++ Programmers/Stack,Queue/Lv2.주식가격.py
@@ -0,0 +1,15 @@
+def solution(prices):
+    answer = []
+
+    for i in range(len(prices)):
+        count = 0
+        for j in range(i + 1, len(prices)):
+            count += 1
+            if prices[i] > prices[j]:
+                break
+        answer.append(count)
+
+    return answer
+
+
+print(solution([1, 2, 3, 2, 3]))
