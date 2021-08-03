96028e90e5a77484d6dfffb1d2b75f73311905f1
Lee ChaeHyeon <chlee1001@naver.com>
2021-08-03 22:37:02 +0900

    21.08.03_BOJ_1789번-수들의 합

--- BOJ/Greedy Algorithm/1789-수들의 합/수들의 합.py
+++ BOJ/Greedy Algorithm/1789-수들의 합/수들의 합.py
@@ -7,8 +7,10 @@ S = int(input())
 N, i = 0, 1
 while True:
     N += i
-    if N >= S:
+    if N > S:
+        print(i - 1)
+        break
+    elif N == S:
+        print(i)
         break
     i += 1
-
-print(i - 1)
