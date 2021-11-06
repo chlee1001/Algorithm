f1bf6355c5366d61ec6a3e31ab4a538a07415081
chlee1001 <chlee1001@naver.com>
2021-11-06 18:01:33 +0900

    Start_Java_Algorithm_BOJ_1000_2021.11.06

---
+++ JAVA_BOJ/boj_1000.java
@@ -0,0 +1,14 @@
+package com.ch;
+
+import java.util.Scanner;
+
+public class boj_1000 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a + b);
+  }
+}
