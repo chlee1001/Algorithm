dea604bef1db3454be0436bfaa51f1220e54f770
chlee1001 <chlee1001@naver.com>
2021-11-06 18:07:20 +0900

    BOJ_1001_2021.11.06

---
+++ JAVA_BOJ/boj_1001.java
@@ -0,0 +1,13 @@
+package com.ch;
+
+import java.util.Scanner;
+
+public class boj_1001 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int a = sc.nextInt();
+    int b = sc.nextInt();
+
+    System.out.println(a - b);
+  }
+}
