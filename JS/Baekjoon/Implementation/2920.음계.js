dd0f6eba1d7c2a2a276f8879543d284912672040
chlee1001 <chlee1001@naver.com>
2022-02-03 23:45:24 +0900

    22.02.03_Baekjoon_2920-음계

---
+++ JS/Baekjoon/Implementation/2920.음계.js
@@ -0,0 +1,22 @@
+const [...input] = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ');
+
+// const result = ['ascending', 'descending', 'mixed'];
+
+let ascending = true;
+let descending = true;
+
+for (let i = 1; i < input.length; i++) {
+    if (Number(input[i]) > Number(input[i - 1])) {
+        descending = false;
+    } else if (Number(input[i]) < Number(input[i - 1])) {
+        ascending = false;
+    }
+}
+
+if (ascending) {
+    console.log('ascending');
+} else if (descending) {
+    console.log('descending');
+} else {
+    console.log('mixed')
+}
